a41797ca127b8bcee5afe42f800302dbc723ad8a
Luca Gherardini <116657982+lucagher@users.noreply.github.com>
2022-10-26 11:33:16 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,219 @@
+// About Me content is the one by default shown
+$('#educationContent').hide();
+$('#publicationsContent').hide();
+$('#experienceContent').hide();
+$('#conferencesContent').hide();
+$('#projectsContent').hide();
+$('#blogContent').hide();
+/* Template
+$('#nameContent').hide();
+*/
+
+$(document).ready(function(){
+
+	$.getJSON("https://api.countapi.xyz/hit/nicolasmeseguer.github.io/634c2142-b35d-430e-b51c-dad16880dd3a", function(response) {
+		$("#contadorVisitas").text(response.value);
+	});
+
+	// Handle 'About Me' content
+	$('#aboutme').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#aboutmeContent');
+		}
+
+	});
+
+	// Handle 'Education' content
+	$('#education').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#educationContent');
+		}
+	});
+
+	// Handle 'Publications' content
+	$('#publications').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#publicationsContent');
+		}
+	});
+
+	// Handle 'Blog' content
+	$('#blog').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#blogContent');
+		}
+	});
+
+	// Handle 'Conferences' content
+	$('#conferences').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#conferencesContent');
+		}
+	});
+
+	// Handle 'Experience' content
+	$('#experience').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#experienceContent');
+		}
+	});
+
+	// Handle 'Projects' content
+	$('#projects').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#projectsContent');
+		}
+	});
+
+	/*
+	// Handle 'Template' content
+	$('#name').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#nameContent');
+		}
+	});
+	*/
+
+	// Whenever you clic on a blog post, you should be redirected to that post' html
+	$('.clickable').click(function(e) {
+		var blogPostURL = window.location.origin + "/blog/" + $(e.currentTarget)[0].childNodes[1].innerText;
+		
+		window.location = blogPostURL;
+	});
+
+	// Copy the citation to the clipboard
+	// THIS SHOULD BE THE SAME FOR ALL THE PAPERS
+	$(document).on("click", "#citation", function(){
+		var text = $(this).parent().parent().next()[0].innerHTML;
+
+		navigator.clipboard.writeText(text);
+
+		toastr.success('Citation copied');
+	});
+
+	// Controls the URL; if it has '#blog'
+	// then trigger the 'Blog' clic
+	if (((window.location).href).substring(((window.location).href).lastIndexOf('#') + 1) == 'blog') {
+		$('#blog').click();
+	}
+
+});
+
+function clearActiveLinks() {
+	$('#navbarList .nav-item .nav-link').each(function() {
+		$(this).removeClass('active');
+	});
+}
+
+function clearActiveDivs() {
+	$('.container .content .active').each(function() {
+		$(this).removeClass('active');
+		$(this).hide();
+	});
+}
+
+function activateLink(e) {
+	$(e.target).addClass('active');
+}
+
+function activateDiv(divId) {
+	$(divId).addClass('active');
+	$(divId).show();
+
+	// Scrolls to the content
+	scrollToContent(divId);
+}
+
+function scrollToContent(divId) {
+	if ($(window).width() < 751) {
+		$('html, body').animate({
+			scrollTop: $(divId).offset().top
+		}, 1);
+	}
+}
+
+function resetViews() {
+	$.getJSON("https://api.countapi.xyz/set/nicolasmeseguer.github.io/634c2142-b35d-430e-b51c-dad16880dd3a?value=0", function(response) {
+		$("#contadorVisitas").text("0");
+	});
+}
